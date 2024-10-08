3f0f53f5de91998b818d370268fc97bc9de91d80
ThaHun <captaindaratsawat@gmail.com>
2024-10-08 15:14:07 +0700

    Final in Final

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -23,7 +23,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link href="{{ route('wishlist.store') }}">
-                        {{ __('wishlist') }}
+                        {{ __('Favorite') }}
                     </x-nav-link>
                 </div>
               </div>
--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -29,55 +29,18 @@
    
 
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Latest Featured Games') }}
+            {{ __('My Favorite') }}
         </h2>
-        <form action="/home/serch" method="get">
-        <div class="input-group">
-            <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" name="serch_box" />
-            <button type="button" class="btn btn-outline-primary" data-mdb-ripple-init>search</button>
-        </div>
-        </form>
+        <form action="/Favorite/search" method="get">
+            <div class="input-group">
+                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" name="serch_box" />
+                <button type="button" class="btn btn-outline-primary" data-mdb-ripple-init>search</button>
+            </div>
+            </form>
     </x-slot>
     
 
-    <div class="flex py-12 ">
-        <!-- Sidebar -->
-        <div class="w-1/4 bg-gray-800 text-white p-6">
-            <h3 class="text-lg font-bold mb-4">Popular Tags</h3>
-            <ul>
-                <ul>
-                    @foreach ($tags as $tag)
-                        @php
-                            $cleanedTag = preg_replace('/{value:(.*?)}/', '$1', $tag->gametype_name);
-                        @endphp
-                        <li>
-                            <a href="{{ url('/search-by-tag/'.$tag->gametype_name) }}">
-                                {{ $cleanedTag }}
-                            </a>
-                        </li>
-                        @endforeach
-        
-                    </ul>
-
-            </ul>
-
-            <a href="#" class="text-orange-500 mt-4 block">See all Tags &rarr;</a>
-
-            <h3 class="text-lg font-bold mt-8 mb-4">Browse</h3>
-            <ul class="space-y-2">
-                <li>Games</li>
-                <li>Tools</li>
-            </ul>
-
-            <h3 class="text-lg font-bold mt-8 mb-4">Games by Price</h3>
-            <ul class="space-y-2">
-                <li>Onsale</li>
-                <li>Free Game</li>
-                <li>With Demo</li>
-                <li>$5 or less</li>
-                <p>Tags:</p>
-            </ul>
-        </div>
+   
     
 
         <!-- Main Content -->
